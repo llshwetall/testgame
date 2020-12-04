d221ed2ff9f2e532c103c6db0e3efcfe51612395
llshwetall <shweta.sahoo@research.iiit.ac.in>
2020-12-05 01:37:30 +0530

    Updates

---
+++ precache-manifest.3a55d6b98655bcf719c7d8f3026d3bfa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ab122f66c58f8d6fdf35a8c85d9f104",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb4c0f2b4b54511b8a5e",
+    "url": "/static/css/2.dec86d71.chunk.css"
+  },
+  {
+    "revision": "a97d181e0aaa8c973eaa",
+    "url": "/static/css/main.b37aab03.chunk.css"
+  },
+  {
+    "revision": "fb4c0f2b4b54511b8a5e",
+    "url": "/static/js/2.2893cd14.chunk.js"
+  },
+  {
+    "revision": "ff91598454b82d07456667b827279715",
+    "url": "/static/js/2.2893cd14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a97d181e0aaa8c973eaa",
+    "url": "/static/js/main.850319fa.chunk.js"
+  },
+  {
+    "revision": "dec26090cf87837f80e5",
+    "url": "/static/js/runtime-main.e95b8414.js"
+  },
+  {
+    "revision": "cc452214905d720d611697975e424112",
+    "url": "/static/media/bg1.cc452214.jpg"
+  },
+  {
+    "revision": "dfbb735ecfbeda595940a26c84620ca1",
+    "url": "/static/media/bg2.dfbb735e.jpg"
+  }
+]);
